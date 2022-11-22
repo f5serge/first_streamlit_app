cffc8bf3edbb8e9fd4654959851009a16d4e5689
f5serge <79228333+f5serge@users.noreply.github.com>
2022-11-23 09:26:03 +1300

    add

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 4 Oatmeal')
+streamlit.text('Kale, Spinach Smoothie')
+streamlit.text('Free-Range Egg')
