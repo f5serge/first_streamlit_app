4133e3d3e1297ea95298c1d0aefba4cecdd333ce
f5serge <79228333+f5serge@users.noreply.github.com>
2022-11-23 09:29:01 +1300

    add csv list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,15 @@
 import streamlit
+import pandas
 
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 4 Oatmeal')
 streamlit.text('Kale, Spinach Smoothie')
 streamlit.text('Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+# https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
